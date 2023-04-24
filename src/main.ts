9795644bd00aadbe45addfa016f716a49dab9e32
Sennay <wsennay@gmail.com>
2023-04-24 13:38:39 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
+  const logger = new Logger('bootstrap');
+  logger.log('server running on port 4000');
 }
 bootstrap();
