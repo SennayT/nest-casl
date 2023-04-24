3fdd141388d3988e67a28a93f96d055aa6594c76
Sennay <wsennay@gmail.com>
2023-04-24 14:07:26 +0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.setGlobalPrefix('v1');
+
   const logger = new Logger('bootstrap');
   logger.log('server running on port 4000');
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('explorer', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
